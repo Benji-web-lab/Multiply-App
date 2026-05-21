011df80c0a963b672b4602068c1ab8dd51b429f9
Benji-web-lab <benginobenjamim007@gmail.com>
2026-05-21 09:57:31 +0200

    New App

--- app/src/main/java/com/example/multiplyapp/MainActivity.kt
+++ app/src/main/java/com/example/multiplyapp/MainActivity.kt
@@ -1,17 +1,39 @@
 package com.example.multiplyapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
 class MainActivity : AppCompatActivity() {
+    val edtNum1 = findViewById<EditText>(R.id.editTextText)
+    val edtNum2 = findViewById<EditText>(R.id.editTextText2)
+    val btnCalc = findViewById<Button>(R.id.button)
+    val txtAns = findViewById<TextView>(R.id.textView)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        btnCalc.setOnClickListener {
+            val num1 = edtNum1.text.toString().toDoubleOrNull() ?: 0.0
+            val num2 = edtNum2.text.toString().toDoubleOrNull() ?: 0.0
+            val result = multiply(num1, num2)
+            txtAns.text = "Answer: $result"
+        }
+    }
+
+    // Function to calculate the product
+    fun multiply(a: Double, b: Double): Double {
+        return a * b
+
+
+
+    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
